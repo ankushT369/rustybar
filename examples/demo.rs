18f8d9cef7640a2392e84f614446cce46d0a81cc
Ankush <mondalankush9851@gmail.com>
2026-01-01 14:44:03 +0530

    Added examples/demo.rs for testing the demo for progress bar

---
+++ examples/demo.rs
@@ -0,0 +1,21 @@
+use rustybar::ProgressBar;
+use std::{thread, time::Duration};
+
+fn main() {
+    let total_size = 50_000;
+
+    let mut bar = ProgressBar::new("Downloading", 40, total_size);
+
+    let mut downloaded = 0;
+
+    while downloaded < total_size {
+        downloaded += 700;
+        if downloaded > total_size {
+            downloaded = total_size;
+        }
+
+        bar.tick(downloaded);
+
+        thread::sleep(Duration::from_millis(80));
+    }
+}
